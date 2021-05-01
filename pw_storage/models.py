e9e5a74f058b66f72c180135e959ee8db7b3a48e
Ricards Taujenis <MozesThegreat@yahoo.com>
2021-05-01 21:22:47 +0200

    Update models.py
    added models

--- pw_storage/models.py
+++ pw_storage/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    GENDER_CHOICES = Choices('male', 'female', 'other')
+    user_id = models.IntegerField(primary_key=True)
+    username = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+    email = models.EmailField(max_length=150)
+    password = models.CharField(max_length=32)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=20)
