81faed4955b08018f0188666b8ad2564e5acb494
Mozes721 <Richard.Taujenis@gmail.com>
2021-05-04 17:18:30 +0200

    forms
    forms field added

---
+++ pw_storage/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from django.forms import fields 
+from .models import User, User_pw 
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+        # widgets = {
+        #     'username': forms.TextInput(attrs={'class': 'form-control'}),
+        #     'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+        #     'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+        #     'email': forms.TextInput(attrs={'class': 'form-control'}),
+        #     'password': forms.CharField(attrs={'class': 'form-control'}),
+        #     'password2': forms.CharField(attrs={'class': 'form-control'}),
+        #     'gender': forms.Select(attrs={'class': 'form-check form-check-inline'}),
+
+        # }
+
+class User_pw_form(forms.ModelForm):
+    class Meta:
+        model = User_pw
+        fields = '__all__'
--- pw_storage/views.py
+++ pw_storage/views.py
@@ -1,27 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 import urllib, hashlib
-from .models import User
-
+from .models import User, User_pw
+from .forms import UserForm
 
 def home_page(request):
     return render(request, 'pw_storage/home.html')
 
 def register_page(request):
-    user = User.objects
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        fname = request.POST.get('fname')
-        lname = request.POST.get('lname')
-        email = request.POST.get('email')
-        password1 = request.POST.get('password1')
-        password2 = request.POST.get('password2')
-        options = request.POST.getlist('inlineOptions')
+    user_form = UserForm
+    context = {'form': user_form}
+    # if request.method == 'POST':
+    #     username = request.POST.get('username')
+    #     fname = request.POST.get('fname')
+    #     lname = request.POST.get('lname')
+    #     email = request.POST.get('email')
+    #     password1 = request.POST.get('password1')
+    #     password2 = request.POST.get('password2')
+    #     options = request.POST.getlist('inlineOptions')
         
-        print(username)
-        print(options)
-    return render(request, 'pw_storage/register.html')
+
+    #     print(username)
+    #     print(options)
+    # template_name = 'register.html'
+    # fields = '__all__'
+    return render(request, 'pw_storage/register.html', context)
 
 def login_page(request):
     return render(request, 'pw_storage/login.html')
