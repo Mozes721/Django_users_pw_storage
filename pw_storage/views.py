6c72822e8fe501bdb822c0d3c607d8ca5414a1b0
Ricards Taujenis <MozesThegreat@yahoo.com>
2021-07-03 21:28:21 +0200

    Register and login view changed in views

--- pw_storage/views.py
+++ pw_storage/views.py
@@ -1,57 +1,56 @@
+from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, response
-from django.template import RequestContext
-import urllib, hashlib
-from .models import User, User_pw
-from .forms import RegistrationForm, LoginForm
-from django.contrib.auth import login, authenticate
-import pdb
-from django.contrib.auth.forms import UserCreationForm
+from django import forms
 from django.contrib import messages
+
+# Create your views here.
+from .forms import LoginForm, RegisterForm
+
+User = get_user_model()
 
 def home_page(request):
     return render(request, 'pw_storage/home.html')
 
 def register_page(request):
-    form = RegistrationForm(request.POST or None)
+    form = RegisterForm(request.POST or None)
     if form.is_valid():
-        username = form.cleaned_data.get('username')
-        firstname = form.cleaned_data.get('firstname')
-        lastname = form.cleaned_data.get('lastname')
-        email = form.cleaned_data.get('email')
-        password = form.cleaned_data.get('password')
-        password1 = form.cleaned_data.get('password1')
-        gender = form.cleaned_data.get('gender')
+        username = form.cleaned_data.get("username")
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password1")
+        password2 = form.cleaned_data.get("password2")
         try:
-            user = User.objects.create_user(request, username=username, firstname=firstname, lastname=lastname, email=email, password=password, gender=gender)
+            user = User.objects.create_user(username, email, password)
         except:
-            user = None 
+            user = None
         if user != None:
-            return redirect("home")
+            login(request, user)
+            return redirect("/")
         else:
-            request.session['register_error'] = 1
-        return render(request, '.pw_storage/user_storage.html')
-    return render(request, 'pw_storage/register.html', {'form': form})
+            request.session['register_error'] = 1 # 1 == True
+    return render(request, "pw_storage/register.html", {"form": form})
 
 
 def login_page(request):
     form = LoginForm(request.POST or None)
     if form.is_valid():
-        username = form.cleaned_data.get('username')
-        password = form.cleaned_data.get('password')
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
         user = authenticate(request, username=username, password=password)
         if user != None:
             # user is valid and active -> is_active
             # request.user == user
             login(request, user)
-            return redirect("user_storage")
+            return redirect("/")
         else:
             # attempt = request.session.get("attempt") or 0
             # request.session['attempt'] = attempt + 1
             # return redirect("/invalid-password")
             request.session['invalid_user'] = 1 # 1 == True
-    return render(request, "login.html", {"form": form})
-
+            messages.warning(request, 'Please enter the right password!')
+    return render(request, "pw_storage/login.html", {"form": form})
+  
 
 def logged_out_page(request):
-    return render(request, 'pw_storage/logged_out.html')
+    logout(request)
+    # request.user == Anon User
+    return redirect("pw_storage/logged_out")
