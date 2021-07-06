7df40eb763a71b76d8352f52bb624c2d813fbeb9
Mozes721 <Richard.Taujenis@gmail.com>
2021-07-06 17:47:03 +0200

    @login_required

--- pw_storage/views.py
+++ pw_storage/views.py
@@ -1,12 +1,16 @@
 from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.shortcuts import render, redirect
-from django import forms
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 from .forms import LoginForm, RegisterForm
 
 User = get_user_model()
+
+if User.is_authenticated():
+    user.request.user:
+
 
 def home_page(request):
     return render(request, 'pw_storage/home.html')
@@ -48,19 +52,21 @@ def login_page(request):
             request.session['invalid_user'] = 1 # 1 == True
             messages.warning(request, 'Please enter the right password!')
     return render(request, "pw_storage/user_account/login.html", {"form": form})
-   
-
+  
+@login_required(login_url=login_page)
 def logged_out_page(request):
     logout(request)
-   
     return render(request, "pw_storage/user_account/logged_out.html")
-
+    g
+@login_required(login_url=login_page)
 def user_pw_all(request):
     return render(request, "pw_storage/user_password/user_pw_all.html")
 
+@login_required(login_url=login_page)
 def user_pw_add(request):
     return render(request, "pw_storage/user_password/user_pw_add.html")
 
+@login_required(login_url=login_page)
 def user_pw_search(request):
     return render(request, "pw_storage/user_password/user_pw_search.html")
 
